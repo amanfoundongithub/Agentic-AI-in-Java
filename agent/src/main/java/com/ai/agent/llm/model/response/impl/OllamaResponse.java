09b39e10456e63457c18b6d57243df9c775ca5fe
Aman Raj <billygates124356@gmail.com>
2025-11-14 23:04:18 +0530

    Updated the Ollama Response with server response

--- agent/src/main/java/com/ai/agent/llm/model/response/impl/OllamaResponse.java
+++ agent/src/main/java/com/ai/agent/llm/model/response/impl/OllamaResponse.java
@@ -1,4 +1,27 @@
 package com.ai.agent.llm.model.response.impl;
 
 public class OllamaResponse {
+
+    public boolean isGenerated;
+
+    public Content content;
+
+    public static class Content {
+
+        public String model;
+        public String created_at;
+
+        public boolean done;
+
+        public String done_reason;
+
+        public Message message;
+
+        public static class Message {
+            public String role;
+            public String content;
+        }
+
+    }
+
 }
