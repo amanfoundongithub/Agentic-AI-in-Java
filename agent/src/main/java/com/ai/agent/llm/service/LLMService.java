65741a197d1a0351095f46914be2d84c1ebc8733
Aman Raj <billygates124356@gmail.com>
2025-11-15 18:41:59 +0530

    Updated with model name

--- agent/src/main/java/com/ai/agent/llm/service/LLMService.java
+++ agent/src/main/java/com/ai/agent/llm/service/LLMService.java
@@ -5,6 +5,8 @@ import com.ai.agent.llm.model.response.LLMResponse;
 
 public interface LLMService<REQ extends LLMRequest, RES extends LLMResponse> {
 
+    String model();
+
     RES generate(REQ request);
 
 }
