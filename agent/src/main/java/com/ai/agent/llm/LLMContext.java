d140ff6cfbb4c506cfb4225e383735f6140f2410
Aman Raj <billygates124356@gmail.com>
2025-11-15 18:42:20 +0530

    Added context of LLM

---
+++ agent/src/main/java/com/ai/agent/llm/LLMContext.java
@@ -0,0 +1,36 @@
+package com.ai.agent.llm;
+
+import com.ai.agent.llm.model.request.LLMRequest;
+import com.ai.agent.llm.model.response.LLMResponse;
+import com.ai.agent.llm.service.LLMService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class LLMContext {
+
+    Map<String, LLMService<?, ?>> llmServiceMap = new HashMap<>();
+
+    @Autowired
+    public LLMContext(List<LLMService<?,?>> llmServices) {
+        for(LLMService<?, ?> service : llmServices) {
+            llmServiceMap.put(service.model(), service);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public<R extends LLMResponse, Q extends LLMRequest>  R generate(String model, Q query) {
+        LLMService<Q, R> llmService = (LLMService<Q, R>) llmServiceMap.get(model);
+
+        if(llmService == null) {
+            throw new IllegalArgumentException("No LLM Service found with the name: " + model);
+        } else {
+            return llmService.generate(query);
+        }
+
+    }
+}
