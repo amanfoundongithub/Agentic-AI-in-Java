3005f68b1d66ee5dedd4ec7455680d715902157b
Aman Raj <billygates124356@gmail.com>
2025-11-15 14:19:48 +0530

    Added description of the tool

--- agent/src/main/java/com/ai/agent/tools/Tool.java
+++ agent/src/main/java/com/ai/agent/tools/Tool.java
@@ -2,6 +2,12 @@ package com.ai.agent.tools;
 
 public interface Tool<RES, REQ>{
 
+    /**
+     * A common interface to execute the action of a tool
+     *
+     * @param request The entity that contains the requested action items for the tool
+     * @return Response from the tool
+     */
     RES execute(REQ request);
 
 }
