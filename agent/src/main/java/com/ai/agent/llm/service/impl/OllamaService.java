345847a3b54b23e8760f2cc507a2b6cea93fd57b
Aman Raj <billygates124356@gmail.com>
2025-11-16 16:21:19 +0530

    Added post construct injection and config in OllamaService

--- agent/src/main/java/com/ai/agent/llm/service/impl/OllamaService.java
+++ agent/src/main/java/com/ai/agent/llm/service/impl/OllamaService.java
@@ -1,28 +1,35 @@
 package com.ai.agent.llm.service.impl;
 
-import com.ai.agent.llm.model.request.impl.OllamaRequest;
-import com.ai.agent.llm.model.response.LLMResponse;
+import com.ai.agent.config.LLMConfig;
+import com.ai.agent.llm.model.LLMResponse;
 import com.ai.agent.llm.model.response.impl.OllamaAPIResponse;
 import com.ai.agent.llm.service.LLMAbstractService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OllamaService extends LLMAbstractService<OllamaRequest, OllamaAPIResponse> {
+public class OllamaService extends LLMAbstractService<OllamaAPIResponse> {
 
-    public OllamaService() {
-        this.url = "http://127.0.0.1:8000/api/llm/generate";
+    @Value("${ollama.api.url}")
+    private String requestUrl;
+
+    @PostConstruct
+    public void init() {
+        this.url = requestUrl;
         this.responseClass = OllamaAPIResponse.class;
     }
 
     @Override
     public String model() {
-        return "llama3.2";
+        return LLMConfig.OLLAMA_MODEL_NAME;
     }
 
     @Override
     protected LLMResponse convert(OllamaAPIResponse response) {
         LLMResponse res = new LLMResponse();
-        res.text = response.content.message.content;
+        res.setGenerated(true);
+        res.setText(response.content.message.content);
         return res;
     }
 
