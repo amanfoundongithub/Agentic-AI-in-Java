5ffcfd9daf7a8a49e04fc29ea0e91e80e38c2c63
Aman Raj <billygates124356@gmail.com>
2025-11-16 15:28:29 +0530

    Added LLMService descriptions

--- agent/src/main/java/com/ai/agent/llm/service/LLMService.java
+++ agent/src/main/java/com/ai/agent/llm/service/LLMService.java
@@ -3,9 +3,21 @@ package com.ai.agent.llm.service;
 import com.ai.agent.llm.model.LLMRequest;
 import com.ai.agent.llm.model.LLMResponse;
 
+/**
+ * Interface for the LLM Service provided via APIs
+ *
+ * @author amanfoundongithub
+ */
 public interface LLMService {
 
     String model();
+
+    /**
+     * Generate the response based on the request
+     *
+     * @param request The request for the LLM containing system and user prompts
+     * @return A response body containing the response from LLM
+     */
     LLMResponse generate(LLMRequest request);
 
 }
