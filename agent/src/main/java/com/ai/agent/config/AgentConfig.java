dc687ff5a35d6b1b4820471579ed67902c2ed349
Aman Raj <billygates124356@gmail.com>
2025-11-16 14:15:51 +0530

    Created a AgentConfig to store agent's config

---
+++ agent/src/main/java/com/ai/agent/config/AgentConfig.java
@@ -0,0 +1,21 @@
+package com.ai.agent.config;
+
+
+/**
+ * A utility class to store the configurations of the agent
+ *
+ *
+ * @author amanfoundongithub
+ *
+ */
+public class AgentConfig {
+
+    // Maximum number of iterations in the agent's loop
+    public static final int MAX_ITERATIONS = 10;
+
+    private AgentConfig() {
+        // To abstract the default public constructor
+    }
+
+    //
+}
