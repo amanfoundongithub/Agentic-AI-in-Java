ab0e532416a0fb1ce918835e03b0c13f9b539ebd
Aman Raj <billygates124356@gmail.com>
2025-11-16 16:50:25 +0530

    Added queryClass() and comments

--- agent/src/main/java/com/ai/agent/mcp/tool/Tool.java
+++ agent/src/main/java/com/ai/agent/mcp/tool/Tool.java
@@ -3,10 +3,22 @@ package com.ai.agent.mcp.tool;
 import com.ai.agent.mcp.tool_query.ToolQuery;
 import com.ai.agent.mcp.tool_result.ToolResult;
 
+/**
+ * A generic interface for tools used in MCP.
+ *
+ * @param <R> The response type returned by the tool.
+ * @param <Q> The query type accepted by the tool.
+ *
+ * @author amanfoundongithub
+ */
 public interface Tool<R extends ToolResult, Q extends ToolQuery>{
 
     String name();
     String description();
+
+    // Class of the query
+    Class<Q> queryClass();
+
     /**
      * A common interface to execute the action of a tool
      *
