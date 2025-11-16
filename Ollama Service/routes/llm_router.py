08cbd97d5002eeda9153000ef4d392cf6f2f52cd
Aman Raj <billygates124356@gmail.com>
2025-11-16 21:31:20 +0530

    Added router for LLM

---
+++ Ollama Service/routes/llm_router.py
@@ -0,0 +1,32 @@
+from fastapi.routing import APIRouter
+from fastapi.responses import JSONResponse
+
+
+from models.LLMGenerationRequest import LLMGenerationRequest
+from service import OllamaLLMService
+
+router = APIRouter()
+
+# LLM Generation Request
+ollama_agent = OllamaLLMService()
+
+
+@router.post("/generate")
+async def generate_llm_response(request : LLMGenerationRequest):
+
+    # Add the system prompt 
+    ollama_agent.set_system_prompt(request.systemPrompt)
+
+    # Generate the response
+    ollama_agent_response = ollama_agent.generate_response(request.prompt)
+
+    if ollama_agent_response.get("isGenerated"):
+        return JSONResponse(
+            status_code = 201, 
+            content = ollama_agent_response
+        )
+    else:
+        return JSONResponse(
+            status_code = 500,
+            content = ollama_agent_response
+        )
