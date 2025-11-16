db672ae28bcac708e62d299b4e8d8ce6608fd667
Aman Raj <billygates124356@gmail.com>
2025-11-16 16:43:55 +0530

    Added exception for not founded LLM services

---
+++ agent/src/main/java/com/ai/agent/exception/LLMNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ai.agent.exception;
+
+public class LLMNotFoundException extends RuntimeException {
+
+    public LLMNotFoundException(String name) {
+        super("No LLM Service found for the name:" + name + ". Please check the name!");
+    }
+
+}
