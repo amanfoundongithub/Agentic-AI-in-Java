c19da46c526a327ad5078b630d138e37ce938375
Aman Raj <billygates124356@gmail.com>
2025-11-20 13:49:47 +0530

    Added config for WebClient

---
+++ agent/src/main/java/com/ai/agent/config/WebClientConfig.java
@@ -0,0 +1,35 @@
+package com.ai.agent.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${web_client.timeoutInSeconds}")
+    private int timeoutInSeconds;
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder
+                .clientConnector(
+                        new ReactorClientHttpConnector(
+                                HttpClient.create()
+                                        .responseTimeout(Duration.ofSeconds(timeoutInSeconds))
+                                        .followRedirect(true)
+                        )
+                )
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+}
