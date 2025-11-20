6171842838de524a2aaf13ad9bdcde93526c5888
Aman Raj <billygates124356@gmail.com>
2025-11-20 18:46:21 +0530

    Abstracted entire method execute() in ToolAbstract

--- agent/src/main/java/com/ai/agent/mcp/tool/ToolAbstract.java
+++ agent/src/main/java/com/ai/agent/mcp/tool/ToolAbstract.java
@@ -2,12 +2,31 @@ package com.ai.agent.mcp.tool;
 
 import com.ai.agent.mcp.tool_query.ToolQuery;
 import com.ai.agent.mcp.tool_result.ToolResult;
+import com.ai.agent.repository.dao.ToolEntityDao;
+import com.ai.agent.repository.document.ToolEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.UUID;
 
 public abstract class ToolAbstract<R extends ToolResult, Q extends ToolQuery> implements Tool<R, Q>{
+
+    @Autowired
+    protected ToolEntityDao toolDao;
+
+    @Autowired
+    protected WebClient webClient;
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(ToolAbstract.class);
 
     protected String name;
     protected String description;
     protected Class<Q> queryClass;
+
+    protected abstract R processingLogic(Q query) throws Exception;
+    protected abstract R processingError();
 
     @Override
     public String name() {
@@ -24,5 +43,45 @@ public abstract class ToolAbstract<R extends ToolResult, Q extends ToolQuery> im
         return queryClass;
     }
 
+    @Override
+    public R execute(Q query, String sessionId) {
+
+        // Log that we received the request
+        LOGGER.info("\tINFO: Request for {} by sessionId: {} Received", name, sessionId);
+
+        // Reference ID
+        String referenceId = UUID.randomUUID().toString();
+        LOGGER.info("\tINFO: Starting tool...(RefId: {})", referenceId);
+
+        // MongoDB entity helper
+        ToolEntity entity = new ToolEntity();
+        entity.setSessionId(sessionId);
+        entity.setReferenceId(referenceId);
+        entity.setName(name);
+        entity.setQuery(query);
+
+        // Result
+        R result;
+
+        try {
+            result = processingLogic(query);
+
+            entity.setStatus("SUCCESS");
+            entity.setResult(result);
+
+        } catch (Exception e) {
+            LOGGER.error("\tERROR: {}", e.getMessage());
+            result = processingError();
+
+            entity.setStatus("FAILURE");
+            entity.setErrorMessage(e.getMessage());
+
+        } finally {
+            LOGGER.info("\tINFO: Request for {} by sessionId: {} Completed", name, sessionId);
+            toolDao.insert(entity);
+        }
+
+        return result;
+    }
 
 }
