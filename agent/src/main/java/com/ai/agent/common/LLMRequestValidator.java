42495b56c68adbe0d374cf5e5012481e49a4e4cd
Aman Raj <billygates124356@gmail.com>
2025-11-20 20:56:40 +0530

    Added LLMRequestValidator for validating LLM request

---
+++ agent/src/main/java/com/ai/agent/common/LLMRequestValidator.java
@@ -0,0 +1,33 @@
+package com.ai.agent.common;
+
+import com.ai.agent.exception.InvalidRequest;
+import com.ai.agent.llm.dto.LLMRequest;
+
+
+
+public class LLMRequestValidator {
+
+    private LLMRequestValidator() {}
+
+    /**
+     * Validates the LLM request, throws an error if it is deemed invalid
+     *
+     * @param request The request to be validated
+     * @throws InvalidRequest if the request is invalid
+     */
+    public static void validate(LLMRequest request) throws InvalidRequest {
+
+        if(request == null) {
+            throw new InvalidRequest("ERR: Missing request. No request is provided. Provide one to generate response");
+        }
+
+        if(request.getRequestId() == null) {
+            throw new InvalidRequest("ERR: Missing requestId. This is needed to track the request's status.");
+        }
+
+        if(request.getPrompt() == null) {
+            throw new InvalidRequest("ERR: Missing prompt. The agent cannot generate response without a prompt from user.");
+        }
+
+    }
+}
