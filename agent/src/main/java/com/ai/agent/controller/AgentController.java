b859becc0286a845c19a9a2705cd82818ccfce5b
Aman Raj <billygates124356@gmail.com>
2025-11-21 10:26:28 +0530

    Added a controller for v1 build

---
+++ agent/src/main/java/com/ai/agent/controller/AgentController.java
@@ -0,0 +1,13 @@
+package com.ai.agent.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1")
+public class AgentController {
+
+}
