1bd311c65263a3877986e381dca818fed17dfb38
Aman Raj <billygates124356@gmail.com>
2025-11-22 12:58:30 +0530

    Created a config for task executor

---
+++ agent/src/main/java/com/ai/agent/core/config/TaskExecutorConfig.java
@@ -0,0 +1,51 @@
+package com.ai.agent.core.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+public class TaskExecutorConfig {
+
+    @Value("${job.pool_size}")
+    private int poolSize;
+
+    @Value("${job.max_pool_size}")
+    private int maxPoolSize;
+
+    @Value("${job.max_queue_size}")
+    private int maxQueueSize;
+
+    @Value("${job.thread_workers_prefix}")
+    private String threadWorkersPrefix;
+
+    @Bean(name = "agentExecutor")
+    public ThreadPoolTaskExecutor llmExecutor() {
+
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+
+        // Set available threads for execution
+        taskExecutor.setCorePoolSize(poolSize);
+        taskExecutor.setMaxPoolSize(maxPoolSize);
+
+        // Queue first architecture
+        taskExecutor.setQueueCapacity(maxQueueSize);
+
+        // For logging purposes
+        taskExecutor.setThreadNamePrefix(threadWorkersPrefix);
+
+        // Graceful shutdown
+        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+
+        // Redirect it to the controller thread if busy
+        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+
+        // Return the executor with config
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+}
