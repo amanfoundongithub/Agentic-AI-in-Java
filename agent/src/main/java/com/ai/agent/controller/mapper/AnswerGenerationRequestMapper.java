1662fe4242f1ec2d9a455552d2292b29796d0894
Aman Raj <billygates124356@gmail.com>
2025-11-22 15:57:54 +0530

    Updated mapper with record class methods

--- agent/src/main/java/com/ai/agent/controller/mapper/AnswerGenerationRequestMapper.java
+++ agent/src/main/java/com/ai/agent/controller/mapper/AnswerGenerationRequestMapper.java
@@ -3,6 +3,13 @@ package com.ai.agent.controller.mapper;
 import com.ai.agent.controller.dto.AnswerRequestDTO;
 import com.ai.agent.core.api.AnswerGenerationRequest;
 
+
+/**
+ * Mapper to convert a request DTO to a request for ReactAgent, using
+ * Adapter pattern.
+ *
+ * @author amanfoundongithub
+ */
 public class AnswerGenerationRequestMapper extends AnswerGenerationRequest {
 
     private final AnswerRequestDTO requestDTO;
@@ -13,16 +20,16 @@ public class AnswerGenerationRequestMapper extends AnswerGenerationRequest {
 
     @Override
     public String getRequestId() {
-        return this.requestDTO.getRequestId();
+        return this.requestDTO.requestId();
     }
 
     @Override
     public String getPrompt() {
-        return this.requestDTO.getPrompt();
+        return this.requestDTO.prompt();
     }
 
     @Override
     public String getSystemPrompt() {
-        return this.requestDTO.getSysPrompt();
+        return this.requestDTO.sysPrompt();
     }
 }
